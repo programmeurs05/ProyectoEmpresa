04084dd15c8cd90dcb36afd9ff0ac56b63e9fd4b
Santiagox <santiagocardenascrs@gmail.com>
2022-08-20 11:56:08 -0500

    Los atributos de la clase empresa

--- Empresa.java
+++ Empresa.java
@@ -3,6 +3,13 @@ package programmeurs.empresa;
 
 public class Empresa {
 
+    private String nomEmp;
+    private String dirEmp;
+    private int telEmp;
+    private int nitEmp;
+    
+
+
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
