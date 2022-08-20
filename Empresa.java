32184b036a2fc5365a0529b796009d4476c04eff
MonicaC <monica_07195@hotmail.com>
2022-08-20 13:13:06 -0500

    Monik07_sabado20

--- Empresa.java
+++ Empresa.java
@@ -3,12 +3,14 @@ package programmeurs.empresa;
 
 public class Empresa {
 
-    private String nomEmp;
-    private String dirEmp;
-    private int telEmp;
-    private int nitEmp;
+    private long id;
+    private String name;
+    private String document;
+    private String phone;
+    private String address;
+    private date createdAt;
+    private date updateAt;
     
-
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
